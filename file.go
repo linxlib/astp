29eebd095735139e31e9042122c9fb77644e68e0
linx <sulinke1133@gmail.com>
2024-08-09 22:25:09 +0800

    fix

--- file.go
+++ file.go
@@ -6,9 +6,9 @@ type File struct {
 	FilePath    string     `json:",omitempty"` //文件路径
 	PackageName string     `json:",omitempty"` //包名
 	PackagePath string     `json:",omitempty"` //包路径
-	Docs        []string   `json:"-"`          //文档
-	Comments    []string   `json:"-"`          //注释
-	Imports     []*Import  `json:"-"`          //导入
+	Docs        []string   `json:",omitempty"` //文档
+	Comments    []string   `json:",omitempty"` //注释
+	Imports     []*Import  `json:",omitempty"` //导入
 	Consts      []*Element `json:",omitempty"` //常量
 	Vars        []*Element `json:",omitempty"` //变量
 	Structs     []*Element `json:",omitempty"` //结构体
